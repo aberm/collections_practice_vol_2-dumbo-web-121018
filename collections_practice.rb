7b1274bdc3c6b09e7d3d2d0a81d2a9aa2d2ba619
Menachem Aber <abermendel@gmail.com>
2018-11-26 20:54:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
-# your code goes here
+# # your code goes here
+
+def begins_with_r(array)
+  array.each do |word|
+    if word.start_with?("r") == false
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.select { |word| word.start_with?("wa") }.first
+end
+
+def remove_non_strings(array)
+  array.select do |item|
+    item.class == String
+  end
+end
+
+def count_elements(array)
+  
+
+
+
