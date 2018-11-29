c9ca0239bc8880706f80239e0a3a733300b3df2a
Menachem Aber <abermendel@gmail.com>
2018-11-29 01:51:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
-# your code goes here
+# # your code goes here
+
+def begins_with_r(array)
+  array.each do |word|
+    if word.start_with?("r") == false
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.each do |word| 
+    if word.to_s.start_with?("wa")
+      return word
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.select do |item|
+    item.class == String
+  end
+end
+
+def count_elements(array)
+
+  # testy = []
+  # array.each do |element|
+  #   element.each do |key, value|
+
+  #         testy[x][:count] += 1
+  #       else
+  #         testy.push({:name => value, :count => 1})
+  #       end
+  #     end
+  #   end
+  # end
+  # testy
+end
+
+def merge_data(keys, data)
+  a = data[0].merge(keys[0])
+end
+
+def find_cool(array)
+  
+end
+
+def organize_schools(array)
+  
+end
+
