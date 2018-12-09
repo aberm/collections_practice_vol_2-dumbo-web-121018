5bf636c0b3f9a07c374df4e290bbfecb5377c906
Menachem Aber <abermendel@gmail.com>
2018-12-09 17:28:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,78 @@
-# your code goes here
+# # your code goes here
+
+require "pry"
+
+def begins_with_r(array)
+  array.each do |word|
+    if word.start_with?("r") == false
+      return false
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.each do |word| 
+    if word.to_s.start_with?("wa")
+      return word
+    end
+  end
+end
+
+def remove_non_strings(array)
+  array.select do |item|
+    item.class == String
+  end
+end
+
+def count_elements(array)
+
+  # testy = []
+  # array.each do |element|
+  #   element.each do |key, value|
+
+  #         testy[x][:count] += 1
+  #       else
+  #         testy.push({:name => value, :count => 1})
+  #       end
+  #     end
+  #   end
+  # end
+  # testy
+end
+
+def merge_data(keys, data)
+  array = []
+  keys.each do |i|
+    array << i
+  end
+  data.each do |k|
+    # binding.pry
+    array.push(k.values)
+  end
+  array
+end
+
+def find_cool(array)
+  array.select do |item|
+    item[:temperature] == "cool"
+  end
+end
+
+def organize_schools(hash)
+  new_hash = {}
+  hash.each do |name, place|
+    new_hash[place[:location]] = []
+  end
+  hash.each do |name, place|
+    new_hash[place[:location]].push(name)
+  end
+  new_hash
+end
+
